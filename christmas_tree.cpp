bd6b902a5cf4c049cfb2dcfe334d5648ccf8f81a
TheArqsz <arek.marcontact@gmail.com>
2020-01-15 17:37:05 +0000

    Fixed typo

--- christmas_tree.cpp
+++ christmas_tree.cpp
@@ -17,7 +17,7 @@ int main()
     short reserved_columns = 2 * rows + 1; // Maximum columns of largest row
     short trunk_width = 3; // Amount of columns in trunk
     short trunk_heigth = 2; // Amount of rows in trunk
-    for(short r=0; r<=rows; r++){
+    for(short r=1; r<=rows; r++){
         short columns = 2*r - 1; // Amount of asterisks in current row
         short amount_of_spaces = (reserved_columns/2) - r;  // Amount of spaces in current row
         cout << setw(amount_of_spaces + 1);
@@ -27,7 +27,7 @@ int main()
         cout << endl;   // Break the line to next row
     }
 
-    for(short r=0; r<trunk_heigth; r++){ // Draw trunk
+    for(short r=1; r<=trunk_heigth; r++){ // Draw trunk
         short amount_of_spaces = (reserved_columns/2) - trunk_width + 1; 
         cout << setw(amount_of_spaces + 1);
         for(short c=0; c<trunk_width; c++){    // Draw | (column by column)
